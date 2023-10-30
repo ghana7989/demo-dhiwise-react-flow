d427a50817e33fc14502c62bba82a09c9b9a3761
Pavan Chindukuri <pavan@192.168.1.3>
2023-10-30 17:16:48 +0530

    adding react flow

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,121 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Canvas from "pages/Canvas";
 import Home from "pages/Home";
 import NotFound from "pages/NotFound";
-const DashboardScreenAlerts = React.lazy(
-  () => import("pages/DashboardScreenAlerts"),
-);
-const DashboardScreenCharts = React.lazy(
-  () => import("pages/DashboardScreenCharts"),
-);
-const DashboardScreenWidgets = React.lazy(
-  () => import("pages/DashboardScreenWidgets"),
-);
-const DashboardScreenPricing = React.lazy(
-  () => import("pages/DashboardScreenPricing"),
-);
-const DashboardScreenProjectsTimeline = React.lazy(
-  () => import("pages/DashboardScreenProjectsTimeline"),
-);
-const DashboardScreenProjectsGeneral = React.lazy(
-  () => import("pages/DashboardScreenProjectsGeneral"),
-);
-const DashboardScreenAccountInvoice = React.lazy(
-  () => import("pages/DashboardScreenAccountInvoice"),
-);
-const DashboardScreenAccountBilling = React.lazy(
-  () => import("pages/DashboardScreenAccountBilling"),
-);
-const DashboardScreenNewUser = React.lazy(
-  () => import("pages/DashboardScreenNewUser"),
-);
-const DashboardScreenReports = React.lazy(
-  () => import("pages/DashboardScreenReports"),
-);
-const DashboardScreenAllProjects = React.lazy(
-  () => import("pages/DashboardScreenAllProjects"),
-);
-const DashboardScreenTeams = React.lazy(
-  () => import("pages/DashboardScreenTeams"),
-);
-const DashboardScreenProfileOverview = React.lazy(
-  () => import("pages/DashboardScreenProfileOverview"),
-);
-const DashboardScreenCRM = React.lazy(() => import("pages/DashboardScreenCRM"));
-const DashboardScreenDefault = React.lazy(
-  () => import("pages/DashboardScreenDefault"),
-);
-const SoftUIDashboardPROThumbnail = React.lazy(
-  () => import("pages/SoftUIDashboardPROThumbnail"),
-);
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
 const ProjectRoutes = () => {
   return (
     <React.Suspense fallback={<>Loading...</>}>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/canvas" element={<Canvas />} />
           <Route path="*" element={<NotFound />} />
-          <Route
-            path="/softuidashboardprothumbnail"
-            element={<SoftUIDashboardPROThumbnail />}
-          />
-          <Route
-            path="/dashboardscreendefault"
-            element={<DashboardScreenDefault />}
-          />
-          <Route path="/dashboardscreencrm" element={<DashboardScreenCRM />} />
-          <Route
-            path="/dashboardscreenprofileoverview"
-            element={<DashboardScreenProfileOverview />}
-          />
-          <Route
-            path="/dashboardscreenteams"
-            element={<DashboardScreenTeams />}
-          />
-          <Route
-            path="/dashboardscreenallprojects"
-            element={<DashboardScreenAllProjects />}
-          />
-          <Route
-            path="/dashboardscreenreports"
-            element={<DashboardScreenReports />}
-          />
-          <Route
-            path="/dashboardscreennewuser"
-            element={<DashboardScreenNewUser />}
-          />
-          <Route
-            path="/dashboardscreenaccountbilling"
-            element={<DashboardScreenAccountBilling />}
-          />
-          <Route
-            path="/dashboardscreenaccountinvoice"
-            element={<DashboardScreenAccountInvoice />}
-          />
-          <Route
-            path="/dashboardscreenprojectsgeneral"
-            element={<DashboardScreenProjectsGeneral />}
-          />
-          <Route
-            path="/dashboardscreenprojectstimeline"
-            element={<DashboardScreenProjectsTimeline />}
-          />
-          <Route
-            path="/dashboardscreenpricing"
-            element={<DashboardScreenPricing />}
-          />
-          <Route
-            path="/dashboardscreenwidgets"
-            element={<DashboardScreenWidgets />}
-          />
-          <Route
-            path="/dashboardscreencharts"
-            element={<DashboardScreenCharts />}
-          />
-          <Route
-            path="/dashboardscreenalerts"
-            element={<DashboardScreenAlerts />}
-          />
         </Routes>
       </Router>
     </React.Suspense>
